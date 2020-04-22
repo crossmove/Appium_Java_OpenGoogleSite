b0ec7246ef497b3aa1d61505d0d7c55aadf49b42
pradeep <pradeep.exe@gmail.com>
2020-04-22 13:30:36 +0400

    automated for Google search

--- src/main/java/test1/ChromeExample.java
+++ src/main/java/test1/ChromeExample.java
@@ -1,9 +1,15 @@
 package test1;
 import org.testng.annotations.Test;
 
+import com.google.common.collect.ImmutableMap;
+
 import java.net.URL;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumDriver;
@@ -13,7 +19,9 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class ChromeExample {
 
-	static AppiumDriver<MobileElement> appDriver;
+	//Any of the below code works fine
+	//static AppiumDriver<MobileElement> appDriver;	
+	static WebDriver appDriver;
 		
 	@Test
 	public static void OpenChromeBrowser() throws Exception {				
@@ -26,6 +34,24 @@ public class ChromeExample {
 		//How to Run
 		//Right click inside any test methods --> Run as --> TestNG Test
 		
+		
+		/*ChromeOptions chrome_options = new ChromeOptions();		
+		chrome_options.addArguments("--disable-infobars");*/
+				
+		/*chrome_options.addArguments(
+		   "--verbose",
+		   //"--headless",
+		   "--disable-web-security",
+		   "--ignore-certificate-errors",
+		   "--allow-running-insecure-content",
+		   "--allow-insecure-localhost",
+		   "--no-sandbox",
+		   "--disable-gpu",
+		   "-incognito",
+		   "--allow-file-access-from-files"
+		  );
+		*/
+		
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");		
@@ -33,23 +59,28 @@ public class ChromeExample {
 		cap.setCapability(MobileCapabilityType.UDID, "575b6cf1");
 		cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "60");
 		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+		cap.setCapability("chromedriverExecutableDir", "D:\\picx\\81");
+		cap.setCapability("appium:chromeOptions", ImmutableMap.of("w3c", false));
 		
+		//cap.setCapability(ChromeOptions.CAPABILITY,chrome_options);		
+		//cap.setCapability("appium:chromeOptions", "{args: ['--disable-popup-blocking']}");			
+		//cap.setCapability("appActivity", "com.google.android.apps.chrome.Main");		
 		//https://chromedriver.storage.googleapis.com/index.html?path=81.0.4044.69/
-		// Should Match the mobile chrome browser version
-		cap.setCapability("chromedriverExecutableDir", "D:\\picx");
-		
+		// Should Match the mobile chrome browser version			
+		//cap.setCapability("showChromedriverLog", True);		
 		//i am facing some issue to change language
 		//cap.setCapability("language", "en"); 
 		//cap.setCapability("locale", "en_US");
-		
-		
+			         
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");		
 		appDriver = new AppiumDriver<MobileElement>(url,cap);		
 		System.out.print("Opened Chrome Browser");
 		
-		appDriver.get("https://www.india.com");
-		appDriver.findElement(By.id("menu-item-1635980"));			
-				
+		appDriver.get("https://www.google.com");		
+		WebElement elem = appDriver.findElement(By.name("q"));
+		//WebElement elem1 = appDriver.findElement(By.name("q"));  this code also works fine
+		elem.sendKeys("learn");
+		elem.sendKeys(Keys.ENTER);				
 	}
 
 }
